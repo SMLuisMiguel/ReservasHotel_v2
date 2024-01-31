24da89c5365429e67e7f8e7b905b51a1cdb10242
Luis_Miguel <lumisamo_1983@hotmail.com>
2024-01-31 10:21:03 +0100

    Realizo fork y pongo mi nombre en el archivo README.

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -7,11 +7,11 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Programa para la Gestión de Hoteles IES Al-Ándalus");
 
+
+
+
+
     }
-
-
-
-
 
 
 }
